a8b57d3023f9f085faba0381ffd5452a16328bcd
GetialJuan <getial.juan@correounivalle.edu.co>
2022-12-10 17:09:43 -0500

    a.ii listo

--- tandplpg.sql
+++ tandplpg.sql
@@ -19,3 +19,27 @@ CREATE TRIGGER valor_incorecto_enrols_grade
     FOR EACH ROW
     EXECUTE FUNCTION exception_enrols_grade();
 
+-- (b) Durante la actualización de un registro si el valor grade es modificado,
+-- usando RAISE NOTICE se debe presentar un mensaje indicando el cambio, si es
+-- igual al valor grade en la tabla se debe indicar que el valor no ha sido
+-- modificado. Si el grade a actualizar es negativo, cero o mayor de cinco use
+-- RAISE EXCEPTION.
+CREATE FUNCTION alter_grade_enrols_function() RETURNS trigger AS
+$$
+BEGIN
+    IF NEW.grade <= 0.00 OR NEW.grade > 5.00 THEN
+        RAISE EXCEPTION 'valor inavalido para grade';
+    END IF;
+    IF NEW.grade = OLD.grade THEN
+        RAISE NOTICE  'grade no cambio';
+    ELSE
+        RAISE NOTICE  'grade cambio';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER alter_grade_enrols
+    AFTER UPDATE ON enrols
+    FOR EACH ROW 
+    EXECUTE FUNCTION alter_grade_enrols_function();
